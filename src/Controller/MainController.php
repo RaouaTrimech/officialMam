3814846d0f122870469e006b98911449c2a55a47
AmaniBchir <amanibchir@insat.u-carthage.tn>
2021-05-06 11:56:17 +0100

    1stcommit

---
+++ src/Controller/MainController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route("",name="mainPage")
+     */
+    public function index(): Response
+    {
+        return $this->render('main/index.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+    /**
+     * @Route("/products",name="productPage")
+     */
+    public function products(): Response
+    {
+        return $this->render('main/products.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+}
